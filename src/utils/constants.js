c209b9f783176ab2dcace7b3de04d420ab993608
tsehelnykalona <tsehelnykalona@outlook.com>
2022-04-19 13:31:22 +0300

    change network

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-import tokenABI from "./BowToken.json";
+import tokenABI from "./Token.json";
 export const tokenAbi = tokenABI.abi;
 export const tokenContractAddress =
-    "0x85d1b81281AEF63122D97b556682Cf9934200b7F";
+    "0xd9D905400b444732B6b47Df12735f9253e44DA06";
